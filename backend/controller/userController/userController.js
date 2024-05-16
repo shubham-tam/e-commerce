8adbf9a98f2c1f541bc7cb634d7089aee1b6eb5e
shubham-tam <tamrakarshubham23@gmail.com>
2024-05-17 00:32:49 +0545

    fix: services separated for user controller, single file upload for user image

--- backend/controller/userController/userController.js
+++ backend/controller/userController/userController.js
@@ -1,78 +1,54 @@
 import expressAsyncHandler from 'express-async-handler';
 
 import {
-    ADD_NEW_USER,
-    GET_USER_BY_ID,
-    DELETE_USER_BY_ID,
-    CHECK_USER_EMAIL_EXISTS,
-    INSERT_IMAGE_PUBLIC_ID_TO_USER_TABLE,
-} from '../../queries/index.js';
-import pool from '../../config/db.js';
-import { cloudinary } from '../../config/cloudinary.js';
-import { generateUniqueUUID, hashPassword } from '../../utils/index.js';
+    addUserToDB,
+    checkIfEmailExists,
+    checkIfUserIdExists,
+    deleteUserByIdService,
+    getUserByIdService,
+    updateUserByIdService,
+    uploadUserPhotoToCloudinary,
+} from './userService.js';
+import { generateUniqueUUID } from '../../utils/index.js';
 
 const registerUser = expressAsyncHandler(async (req, res) => {
-    const {
-        first_name,
-        middle_name,
-        last_name,
-        email,
-        password,
-        address,
-        phone_number,
-        payment_mode,
-    } = req.body;
-
     try {
-        const emailCheckResults = await pool.query(CHECK_USER_EMAIL_EXISTS, [email]);
+        const id = generateUniqueUUID();
 
-        if (emailCheckResults?.rows?.length) {
-            return res.status(400).send('Email already exists');
+        const [emailExists, emailError] = await checkIfEmailExists(req.body.email);
+        if (emailError) {
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        if (emailExists) {
+            return res.status(400).send({ message: 'Email already exists' });
         }
 
-        const id = generateUniqueUUID();
-        const hashedPassword = password ? await hashPassword(password) : '';
+        const [addUserSuccess, addUserError] = await addUserToDB(req.body, id);
+        if (addUserError) {
+            return res.status(500).json({ error: 'Internal server error' });
+        }
 
-        await pool.query(ADD_NEW_USER, [
-            id,
-            first_name,
-            middle_name,
-            last_name,
-            email,
-            hashedPassword,
-            address,
-            phone_number,
-            payment_mode,
-        ]);
-
-        const getUserById = await pool.query(GET_USER_BY_ID, [id]);
-
-        if (getUserById?.rows?.length === 0) {
+        const [getUserResult, getUserError] = await checkIfUserIdExists(id);
+        if (getUserError) {
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        if (getUserResult?.rows?.length === 0) {
             return res.status(400).send({ message: 'User does not exist' });
         }
 
-        if (getUserById?.rows?.length >= 1) {
-            cloudinary?.uploader?.upload(req?.files?.[0]?.path, function (err, result) {
-                if (err) {
-                    console.log(err);
-                    return res.status(500).json({
-                        success: false,
-                        message: 'Error',
-                    });
-                }
-
-                if (result) {
-                    // update db with result public_id
-                    pool.query(
-                        INSERT_IMAGE_PUBLIC_ID_TO_USER_TABLE,
-                        [result?.public_id, id],
-                        err => {
-                            if (err) throw err;
-                        }
-                    );
-                    res.status(201).send('User created successfully');
-                }
-            });
+        if (getUserResult?.rows?.length >= 1 && req?.files?.[0]?.path) {
+            const [uploadSuccess, uploadError] = await uploadUserPhotoToCloudinary(
+                req.files[0].path,
+                id
+            );
+            if (uploadError) {
+                return res.status(500).json({ error: 'Internal server error' });
+            }
+            if (uploadSuccess) {
+                return res.status(201).send('User created successfully');
+            }
+        } else {
+            return res.status(201).send('User created successfully');
         }
     } catch (error) {
         console.error('Error:', error);
@@ -83,38 +59,22 @@ const registerUser = expressAsyncHandler(async (req, res) => {
 const updateUserById = expressAsyncHandler(async (req, res) => {
     const id = req?.params?.id;
 
-    const fieldsToUpdate = req.body;
-
     try {
-        const checkIfUserExits = await pool.query(GET_USER_BY_ID, [id]);
-
+        const [checkIfUserExits, checkUserError] = await checkIfUserIdExists(id);
+        if (checkUserError) {
+            return res.status(500).json({ error: 'Internal server error' });
+        }
         if (checkIfUserExits?.rows?.length === 0) {
             return res.status(400).send({ message: 'User does not exist' });
         }
 
-        const updateFields = [];
-        const updateValues = [];
-
-        Object.entries(fieldsToUpdate).forEach(([key, value]) => {
-            if (value !== undefined && key !== 'id') {
-                updateFields.push(key);
-                updateValues.push(value);
-            }
-        });
-
-        updateValues.push(id);
-
-        const updateQuery = `
-            UPDATE "user" 
-            SET ${updateFields.map((field, index) => `${field} = $${index + 1}`).join(', ')} 
-            WHERE id = $${updateValues.length}
-        `;
-
-        await pool.query(updateQuery, updateValues);
+        const [updateSuccess, updateError] = await updateUserByIdService(id, req);
+        if (updateError) {
+            return res.status(500).json({ error: 'Internal server error' });
+        }
 
         return res.status(200).send({ message: 'User updated successfully' });
     } catch (error) {
-        console.error('Error:', error);
         res.status(500).send('Internal Server Error');
     }
 });
@@ -122,78 +82,26 @@ const updateUserById = expressAsyncHandler(async (req, res) => {
 const getUserById = expressAsyncHandler(async (req, res) => {
     const id = req?.params?.id;
 
-    try {
-        pool.query(GET_USER_BY_ID, [id], async (err, results) => {
-            if (err) {
-                console.error('Error executing query:', err);
-                return res.status(500).json({ error: 'Internal server error' });
-            }
+    const [data, error] = await getUserByIdService(id);
 
-            const filteredData = results.rows.map(result => {
-                const { password, ...rest } = result;
-                return rest;
-            });
-
-            let formData = { ...filteredData?.[0] };
-
-            if (filteredData?.[0]?.image_id) {
-                try {
-                    const imageDetails = await cloudinary.api.resource(filteredData?.[0]?.image_id);
-
-                    const {
-                        asset_id,
-                        public_id,
-                        format,
-                        resource_type,
-                        bytes,
-                        width,
-                        height,
-                        url,
-                        secure_url,
-                    } = imageDetails;
-
-                    const image_id = {
-                        asset_id,
-                        public_id,
-                        format,
-                        resource_type,
-                        bytes,
-                        width,
-                        height,
-                        url,
-                        secure_url,
-                    };
-
-                    formData.image_id = image_id;
-                } catch (error) {
-                    console.error(error);
-                    // Handle error fetching image details
-                }
-            }
-
-            res.status(200).json({ data: formData });
-        });
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Internal Server Error');
+    if (error) {
+        return res.status(500).json({ error: 'Internal server error' });
     }
+
+    res.status(200).json({ data });
 });
 
 const deleteUserById = expressAsyncHandler(async (req, res) => {
     const id = req?.params?.id;
 
-    try {
-        pool.query(DELETE_USER_BY_ID, [id], (err, results) => {
-            if (err) {
-                console.error('Error executing query:', err);
-                return res.status(500).json({ error: 'Internal server error' });
-            }
+    const [success, error] = await deleteUserByIdService(id);
 
-            res.status(200).json({ message: 'User deletion successfull' });
-        });
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Internal Server Error');
+    if (error) {
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+
+    if (success) {
+        res.status(200).json({ message: 'User deletion successful' });
     }
 });
 
--- backend/utils/index.js
+++ backend/utils/index.js
@@ -1,7 +1,14 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export const generateUniqueUUID = () => {
-    return uuidv4();
+    return uuidv4().replace(/-/gi, '');
+};
+
+export const checkSingleFile = (req, res, next) => {
+    if (req.files && req.files.length > 1) {
+        return res.status(400).json({ error: 'Only one file can be uploaded at a time' });
+    }
+    next();
 };
 
 export * from './passwords.js';
